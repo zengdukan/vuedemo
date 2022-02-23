f83f9bd4036ef32fb7de759d9ad303acb73342f7
zengdukan <zengdukan@163.com>
2022-02-23 15:33:45 +0800

    mapState

--- src/Counter.vue
+++ src/Counter.vue
@@ -1,10 +1,19 @@
 <template>
     <button @click="increment">{{ count }}</button>
+    <button @click="increment">{{ countAlias }}</button>
+    <button @click="increment">{{ countPlusLocalState }}</button>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
+    data() {
+        return {
+            localCount: 1,
+        };
+    },
+
     methods: {
         increment() {
             this.$store.commit('increment');
@@ -12,10 +21,12 @@ export default {
         }
     },
 
-    computed: {
-        count() {
-            return this.$store.state.count;
+    computed: mapState({
+        count: state => state.count,
+        countAlias: 'count',
+        countPlusLocalState (state) {
+            return state.count + this.localCount;
         }
-    }
+    }),
 }
 </script>
